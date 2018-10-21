0c606d35adfafbd6469fec7e157f7fef9a92801e
Kieran <me@kieranajp.co.uk>
2018-10-21 02:13:51 +0200

    Router tests

---
+++ spec/Infrastructure/Router/FractalStrategy.spec.php
@@ -0,0 +1,41 @@
+<?php declare(strict_types=1);
+
+use BlueBook\Infrastructure\Router\FractalStrategy;
+use Http\Factory\Diactoros\ResponseFactory;
+use League\Container\Container;
+use League\Fractal\Resource\Collection;
+use League\Route\Route;
+use Psr\Http\Message\ResponseInterface;
+use Zend\Diactoros\ServerRequest;
+
+describe(FractalStrategy::class, function () {
+
+    it('transforms a Resource', function () {
+        $responseFactory = new ResponseFactory();
+        $strategy = new FractalStrategy($responseFactory);
+        $container = new Container();
+
+        allow($strategy)->toReceive('getContainer')
+            ->andReturn($container);
+
+        allow($strategy)->toReceive('transformResourceToArray')
+            ->andReturn([ 'hello' => 'banana' ]);
+
+        $route = new Route('POST', '/banana', 'BananaHandler');
+        $request = new ServerRequest;
+
+        allow($route)->toReceive('getVars')->andReturn([]);
+        allow($route)->toReceive('getCallable')->andReturn(function ($request, $vars) {
+            return new Collection(['hello' => 'banana']);
+        });
+
+        $result = $strategy->invokeRouteCallable($route, $request);
+
+        expect($result)->toBeAnInstanceOf(ResponseInterface::class);
+
+
+        expect((string) $result->getBody())->toContain('banana');
+
+    });
+
+});
