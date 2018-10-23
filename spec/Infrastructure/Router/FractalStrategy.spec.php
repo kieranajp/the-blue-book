6dc4ac48dcd33c2a0da8f2604def710bd3943fe3
Kieran <me@kieranajp.co.uk>
2018-10-23 22:31:59 +0200

    Test coverage

--- spec/Infrastructure/Router/FractalStrategy.spec.php
+++ spec/Infrastructure/Router/FractalStrategy.spec.php
@@ -3,6 +3,7 @@
 use BlueBook\Infrastructure\Router\FractalStrategy;
 use Http\Factory\Diactoros\ResponseFactory;
 use League\Container\Container;
+use League\Fractal\Manager;
 use League\Fractal\Resource\Collection;
 use League\Route\Route;
 use Psr\Http\Message\ResponseInterface;
@@ -10,10 +11,35 @@ use Zend\Diactoros\ServerRequest;
 
 describe(FractalStrategy::class, function () {
 
+    it('converts an array to a JSON response', function () {
+         $responseFactory = new ResponseFactory;
+         $strategy = new FractalStrategy($responseFactory);
+         $container = new Container;
+
+         allow($strategy)->toReceive('getContainer')
+             ->andReturn($container);
+
+         $route = new Route('POST', '/banana', 'BananaHandler');
+         $request = new ServerRequest;
+
+         allow($route)->toReceive('getVars')
+             ->andReturn([]);
+         allow($route)->toReceive('getCallable')
+             ->andReturn(function ($request, $vars) {
+                 return ['hello', 'banana'];
+             });
+
+         $result = $strategy->invokeRouteCallable($route, $request);
+
+        expect($result)->toBeAnInstanceOf(ResponseInterface::class);
+
+        expect((string) $result->getBody())->toContain('banana');
+    });
+
     it('transforms a Resource', function () {
-        $responseFactory = new ResponseFactory();
+        $responseFactory = new ResponseFactory;
         $strategy = new FractalStrategy($responseFactory);
-        $container = new Container();
+        $container = new Container;
 
         allow($strategy)->toReceive('getContainer')
             ->andReturn($container);
@@ -24,15 +50,16 @@ describe(FractalStrategy::class, function () {
         $route = new Route('POST', '/banana', 'BananaHandler');
         $request = new ServerRequest;
 
-        allow($route)->toReceive('getVars')->andReturn([]);
-        allow($route)->toReceive('getCallable')->andReturn(function ($request, $vars) {
-            return new Collection(['hello' => 'banana']);
-        });
+        allow($route)->toReceive('getVars')
+            ->andReturn([]);
+        allow($route)->toReceive('getCallable')
+            ->andReturn(function ($request, $vars) {
+                return new Collection(['hello' => 'banana']);
+            });
 
         $result = $strategy->invokeRouteCallable($route, $request);
 
         expect($result)->toBeAnInstanceOf(ResponseInterface::class);
-
 
         expect((string) $result->getBody())->toContain('banana');
 
